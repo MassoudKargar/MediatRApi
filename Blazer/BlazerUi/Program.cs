8e414e4058b7c8af51f89d58119bd0ed2eb354ff
MassoudKargar <75643194+MassoudKargar@users.noreply.github.com>
2022-01-27 03:02:24 +0330

    Create Common Layer

--- Blazer/BlazerUi/Program.cs
+++ Blazer/BlazerUi/Program.cs
@@ -1,12 +1,4 @@
-using BlazerUi.Areas.Identity;
-using BlazerUi.Data;
 
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,7 +24,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
---
+++ Blazer/BlazerUi/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using BlazerUi.Areas.Identity;
+global using BlazerUi.Data;
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.UI;
+global using Microsoft.EntityFrameworkCore;
